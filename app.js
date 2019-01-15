d96f921ef76b1c34dfe2acbf9c1bd11532531514
texonth <vyatkin.fo@gmail.com>
2019-01-15 16:25:42 +0300

    #6 Add User model

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user')
 
 const app = express();
 
@@ -20,6 +22,12 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
@@ -27,12 +35,19 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
+            users: [User!]!
         }
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
         schema {
             query: RootQuery
@@ -57,17 +72,56 @@ app.use('/graphql', graphqlHttp({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+                date: new Date(args.eventInput.date),
+                creator: '5c3dd9c3479645404c271261'
             });
-            return event.save().then(result => {
-                console.log(result);
-                return {...result._doc, _id: result.id}
-            }).catch(err => {
+            let createdEvent;
+            return event
+                .save()
+                .then(result => {
+                createdEvent =  {...result._doc, _id: result.id}
+                return User.findById('5c3dd9c3479645404c271261');
+
+            })
+            .then(user => {
+                if (!user) {
+                    throw new Error('User not found.')
+                }
+                user.createdEvents.push(event);
+                return user.save();
+            }).then( () => {
+                return createdEvent;
+            })
+            .catch(err => {
                 console.log(err);
                 throw err;
             });
             
         },
+        createUser: args => {
+            return User.findOne({email: args.userInput.email})
+            .then(user => {
+                if (user) {
+                    throw new Error('User exists already.')
+                }
+                return bcrypt.hash(args.userInput.password, 12)
+            })
+            .then(hashedPassword => {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: hashedPassword
+                });
+
+                return user.save();
+            })
+            .then(result => {
+                console.log(result);
+                return {...result._doc, password: null, _id: result.id};
+            })
+            .catch(err => {
+                throw err;
+            });
+        }
     },
     graphiql: true
 }));
@@ -75,7 +129,7 @@ app.use('/graphql', graphqlHttp({
 mongoose.connect(
     `mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PASSWORD
-    }@cluster0-g9tbl.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`).then(() => {
+    }@txn-cluster-1-jmssj.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`).then(() => {
     app.listen(3000);
     console.log("APP STARTED");
 }).catch(err => {
