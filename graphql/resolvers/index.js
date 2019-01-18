921a49bed2621f1492c30e6bbe8e756b2212859e
texonth <vyatkin.fo@gmail.com>
2019-01-18 15:00:45 +0300

    Relations in queries, refactoring

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,121 @@
+const bcrypt = require('bcryptjs');
+const Event = require('../../models/event');
+const User = require('../../models/user')
+
+
+
+const user = (userId) => {
+  return User.findById(userId)
+    .then(user => {
+      return { ...user._doc,
+        _id: user.id,
+        createdEvents: events.bind(this, user._doc.createdEvents)
+      };
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({
+      _id: {
+        $in: eventIds
+      }
+    })
+    return events.map(event => {
+      return { ...event._doc,
+        _id: event.id,
+        creator: user.bind(this, event._doc.creator)
+      };
+    })
+  } catch (err) {
+    throw err;
+  };
+}
+
+
+module.exports = {
+  events: () => {
+    return Event.find()
+      .then(events => {
+        return events.map(event => {
+          return {
+            ...event._doc,
+            _id: event.id,
+            date: new Date(event._doc.date).toISOString(),
+            creator: user.bind(this, event._doc.creator)
+          }; //event.id is shortcut for event._doc._id.toString()
+
+        })
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  createEvent: (args) => {
+
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '5c3dd9c3479645404c271261'
+    });
+    let createdEvent;
+    return event
+      .save()
+      .then(result => {
+        createdEvent = { ...result._doc,
+          _id: result.id,
+          creator: user.bind(this, result._doc.creator)
+        }
+        return User.findById('5c3dd9c3479645404c271261');
+
+      })
+      .then(user => {
+        if (!user) {
+          throw new Error('User not found.')
+        }
+        user.createdEvents.push(event);
+        return user.save();
+      }).then(() => {
+        return createdEvent;
+      })
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
+
+  },
+  createUser: args => {
+    return User.findOne({
+        email: args.userInput.email
+      })
+      .then(user => {
+        if (user) {
+          throw new Error('User exists already.')
+        }
+        return bcrypt.hash(args.userInput.password, 12)
+      })
+      .then(hashedPassword => {
+        const user = new User({
+          email: args.userInput.email,
+          password: hashedPassword
+        });
+
+        return user.save();
+      })
+      .then(result => {
+        console.log(result);
+        return { ...result._doc,
+          password: null,
+          _id: result.id
+        };
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+}
--- models/user.js
+++ models/user.js
@@ -2,19 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    createdEvents: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Event'
-    }]
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdEvents: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Event'
+  }]
 });
 
 module.exports = mongoose.model('User', userSchema);
-
